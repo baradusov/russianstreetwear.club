777debfc3dd26588cc10b048f04588791ced905b
baradusov <baradusovnh@gmail.com>
2020-02-28 21:05:07 +0400

    Добавил поиск

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import Layout from "../components/Layout";
 import * as data from "./api/data.json";
 
 const Index = ({ brands }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searhResults, setSearchResults] = useState([]);
+  const handleChange = ({ target }) => {
+    setSearchTerm(target.value);
+  };
+
+  useEffect(() => {
+    const results = brands.filter(({ name }) => {
+      const brandName = name.toLowerCase();
+      return brandName.includes(searchTerm.toLowerCase());
+    });
+
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <Layout>
-      <CardList data={brands} />
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Поиск"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <CardList data={searhResults} />
     </Layout>
   );
 };
