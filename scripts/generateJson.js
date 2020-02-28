3cc5bcb8835fae383c6122d510a6bf6d1f14f257
baradusov <baradusovnh@gmail.com>
2020-02-28 21:06:44 +0400

    Добавил простую генерацию uuid

--- scripts/generateJson.js
+++ scripts/generateJson.js
@@ -10,11 +10,15 @@ const PATH_OUTPUT = path.resolve(__dirname, "../pages/api/data.json");
 const brands = [];
 
 const yamls = fs.readdirSync(PATH_BRANDS);
+const uuid = () => `${Math.floor(Math.random() * 999999)}-${Date.now()}`;
 
 yamls.forEach(yamlFile => {
   const json = yaml.load(fs.readFileSync(`${PATH_BRANDS}/${yamlFile}`));
 
-  brands.push(json);
+  brands.push({
+    ...json,
+    id: uuid()
+  });
 });
 
 // TODO: sorting json
