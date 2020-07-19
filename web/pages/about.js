b16df7fa66929528728cd77b09b7ffb401c31d1e
Nuril Baradusov <baradusovnh@gmail.com>
2020-07-19 15:48:51 +0400

    Изменил форму пожертвования
    Добавил поле для комментариев и способ пожертвования через банковскую карту

--- web/pages/about.js
+++ web/pages/about.js
@@ -41,9 +41,9 @@ const About = ({ quantity }) => {
 
       <h2>Поддержать</h2>
       <iframe
-        src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0%20%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B0%20%C2%AB%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D1%85%20%D0%B1%D1%80%D0%B5%D0%BD%D0%B4%D0%BE%D0%B2%20%D1%83%D0%BB%D0%B8%D1%87%D0%BD%D0%BE%D0%B9%20%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D1%8B%C2%BB&targets-hint=&default-sum=100&button-text=14&hint=&successURL=https%3A%2F%2Frussianstreetwear.now.sh%2F&quickpay=shop&account=41001432509428"
+        src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0%20%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B0%20%C2%AB%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D1%85%20%D0%B1%D1%80%D0%B5%D0%BD%D0%B4%D0%BE%D0%B2%20%D1%83%D0%BB%D0%B8%D1%87%D0%BD%D0%BE%D0%B9%20%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D1%8B%C2%BB&targets-hint=&default-sum=100&button-text=14&payment-type-choice=on&comment=on&hint=&successURL=https%3A%2F%2Frussianstreetwear.club&quickpay=shop&account=41001432509428"
         width="100%"
-        height="250"
+        height="329"
         frameBorder="0"
         allowtransparency="true"
         scrolling="no"
